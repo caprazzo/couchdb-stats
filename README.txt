bab506e7a9d1d27e1ffac0d803d4a61fe7861f5e
mcaprari <matteo.caprari@gmail.com>
2010-01-04 12:52:17 +0000

    readme

--- README.txt
+++ README.txt
@@ -1,3 +1,43 @@
 stats visualization for couchdb
 ===============================
 
+Setup stats tracker
+-------------------
+
+python stats_copy.py <server_to_track> <target_server> <target_database> <stats range>
+To store localhost couchdb stats each minute on localhost/stats use
+$ python stats_copy.py localhost localhost stats 60 &disown
+
+To stop, kill the process.
+
+Install couchapp app/ in the same database where the stats are being collected.
+
+Views
+=====
+
+_list/table/all_http_requests
+-----------------------------
+
+Displays a chronoscope view of all http requests 
+
+MANDATORY group_level 1-6
+OPTIONAL table_only [true|false] to enable/disable chronoscope
+
+group_level
+	1 BY YEAR
+	2 BY MONTH
+	3 BY DAY
+	4 BY HOUR
+	5 BY 5-MINUTES
+	6 BY MINUTE
+	
+Standard couchdb range queries can be used to limit the results
+
+Examples
+Chronoscope view by the hour:
+http://caprazzi.net:5984/stats/_design/app/_list/table/all_http_requests?group_level=4
+
+Table view by the day:
+http://caprazzi.net:5984/stats/_design/app/_list/table/all_http_requests?group_level=3&table_only=true
+
+	
--- app/lists/table.js
+++ app/lists/table.js
@@ -3,7 +3,8 @@
 	builds an html table conformant to the timepedia chronoscope microformat
 	(http://timepedia.org/chronoscope/docs/microformats/).
 	
-	Invoke with ?version=0.10.x if stats generated from a 0.10.x couchdb server
+	MANDATORY group_level 1-6 to control grouping
+	OPTIONAL table_only [true|false] to disable/enable chronoscope (default enabled)
 */
 function(head, req) {
 	// !json templates
@@ -11,12 +12,11 @@ function(head, req) {
 	
  	start({"headers":{"Content-Type" : "text/html"}});
 	
-	
 	// group_level 6
 	// key is rebuilt skipping element 4 as it's a 5-minutes span and
 	// is not understood by chronoscope dtformat (better done in the template?)
 	
-	
+	var table_only = (req.query.table_only && req.query.table_only=='true');
 	var f_key = function(key) {
 		return [ key[0], key[1], key[2], key[3], key[5] ].join('-')
 	}
@@ -32,7 +32,7 @@ function(head, req) {
 		}
 		dtformat = 'yyyy-M-d-H-m'.split('-').slice(0,parseInt(req.query.group_level)).join('-');
 	}
-	if (!req.query.table_only)
+	if (!table_only)
 		send(Mustache.to_html(templates.chronoscope.html_head));
 		
 	send(Mustache.to_html(templates.chronoscope.table_head, {dtformat:dtformat}));
@@ -45,6 +45,6 @@ function(head, req) {
 	}
 	send(Mustache.to_html(templates.chronoscope.table_foot));
 	
-	if (!req.query.table_only)
+	if (!table_only)
 		send(Mustache.to_html(templates.chronoscope.html_foot));
 }
